6f7621adb2d3046bd183f031da0b6f734e5013e1
wesley <wesleycaldeiradeoliveira@gmail.com>
2024-01-11 14:30:15 -0300

    Associação Product e OrderItem muitos para muitos

--- src/main/java/com/wesleydev/Web/entities/OrderItem.java
+++ src/main/java/com/wesleydev/Web/entities/OrderItem.java
@@ -39,7 +39,8 @@ public class OrderItem implements Serializable {
 	public void setOrder(Order order) {
 		id.setOrder(order);
 	}
-
+	
+	
 	public Product getProduct() {
 		return id.getProduct();
 	}
