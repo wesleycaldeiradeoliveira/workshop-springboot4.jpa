a9b3dd1b54c324ad00ff8823381281933cb46494
wesley <wesleycaldeiradeoliveira@gmail.com>
2024-01-18 10:56:30 -0300

    Tratamento de exceção update

--- src/main/java/com/wesleydev/Web/services/UserService.java
+++ src/main/java/com/wesleydev/Web/services/UserService.java
@@ -13,6 +13,8 @@ import com.wesleydev.Web.repositories.UserRepository;
 import com.wesleydev.Web.services.exceptions.DatabaseExceptions;
 import com.wesleydev.Web.services.exceptions.ResourceNotFoundException;
 
+import jakarta.persistence.EntityNotFoundException;
+
 @Service
 public class UserService {
 	@Autowired
@@ -44,9 +46,14 @@ public class UserService {
 	}
 
 	public User update(Long id, User obj) {
-		User entity = repository.getReferenceById(id);
-		updateData(entity, obj);
-		return repository.save(entity);
+		try {
+			User entity = repository.getReferenceById(id);
+			updateData(entity, obj);
+			return repository.save(entity);
+		} catch (EntityNotFoundException e) {
+			throw new ResourceNotFoundException(id);
+		}
+
 	}
 
 	private void updateData(User entity, User obj) {
